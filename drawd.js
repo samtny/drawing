6e10383113dc7325377100327f2a41ddf19427a6
Sam T <sthompson@amilink.com>
2014-11-11 12:06:50 -0500

    toolbox

--- drawd.js
+++ drawd.js
@@ -3,89 +3,54 @@
 var
   http = require('http'),
   url = require('url'),
-  path = require('path'),
-  fs = require('fs'),
-  _ = require('underscore'),
-  DrawingResponse = require('./lib/DrawingResponse'),
-  DrawingError = require('./lib/DrawingError'),
   Canvas = require('./lib/Canvas'),
-  Line = require('./lib/Line'),
-  Rectangle = require('./lib/Rectangle'),
-  Bucket = require('./lib/Bucket');
+  ToolBox = require('./lib/ToolBox'),
+  DrawingError = require('./lib/DrawingError');
 
 var canvas = null;
 
 var drawd = function (req, res) {
   var drawd = this,
     uri = url.parse(req.url).pathname,
-    cwd = process.cwd();
+    toolbox = new ToolBox(),
+    fs = require('fs'),
+    path = require('path');
 
-  //console.log('uri:', uri);
-
-  drawd.respond = function (body, code, ctype, binary) {
+  drawd.respond = function (body, code, ctype) {
     code = code || 200;
     ctype = ctype || 'application/json';
     body = ctype == 'application/json' ? JSON.stringify(body) : body;
 
-    if (binary === true) {
-      //res.writeHead(200);
-      //res.write(body, 'binary');
-      //res.end();
-
-      res.writeHead(code, { 'Content-Type': 'text/html' });
-      res.end(body);
-    } else {
-      res.writeHead(code, { 'Content-Type': ctype });
-      res.end(body);
-    }
+    res.writeHead(code, { 'Content-Type': ctype });
+    res.end(body);
   };
 
-  drawd.updateCanvas = function (cmd) {
+  drawd.doCommand = function (commandString) {
     try {
-      var args = cmd.toString().split(' ');
+      var explode = commandString.toString().trim().split(' '),
+        command = explode[0].toUpperCase(),
+        args = explode.slice(1);
 
-      switch (args[0].toUpperCase()) {
+      switch (command) {
         case 'C':
-          canvas = new Canvas(args.slice(1));
+          canvas = new Canvas(args);
           drawd.respond(canvas);
-          break;
-        case 'L':
-          if (canvas != null) {
-            var line = new Line(args.slice(1));
 
-            line.draw(canvas);
-            drawd.respond(canvas);
-          }
-          else {
-            drawd.respond('Create a new Canvas first', 400);
-          }
-          break;
-        case 'R':
-          if (canvas != null) {
-            var rectangle = new Rectangle(args.slice(1));
-
-            rectangle.draw(canvas);
-            drawd.respond(canvas);
-          }
-          else {
-            drawd.respond('Create a new Canvas first', 400);
-          }
-          break;
-        case 'B':
-          if (canvas != null) {
-            var bucket = new Bucket(args.slice(1));
-
-            bucket.draw(canvas);
-            drawd.respond(canvas);
-          }
-          else {
-            drawd.respond('Create a new Canvas first', 400);
-          }
           break;
         case 'Q':
-          drawd.respond('', 200);
+          canvas = null;
+
           break;
         default:
+          if (canvas !== null) {
+            toolbox.createTool(command, args).draw(canvas);
+
+            drawd.respond(canvas);
+          }
+          else {
+            drawd.respond('Create a new Canvas first', 400, 'text/plain');
+          }
+
           break;
       }
     }
@@ -99,9 +64,8 @@ var drawd = function (req, res) {
     }
   };
 
+  // original idea here; https://gist.github.com/rpflorence/701407
   drawd.serveFile = function (filename) {
-    // original idea here; https://gist.github.com/rpflorence/701407
-
     fs.exists(filename, function(exists) {
       if(!exists) {
         drawd.respond('404 Not Found\n', 404, 'text/plain');
@@ -118,7 +82,7 @@ var drawd = function (req, res) {
           return;
         }
 
-        drawd.respond(file, 200, 'text/html', true);
+        drawd.respond(file, 200, path.extname(filename) === '.html' ? 'text/html' : 'text/plain', true);
       });
     });
   };
@@ -127,10 +91,11 @@ var drawd = function (req, res) {
     switch (uri) {
       case '/canvas':
         drawd.respond(canvas);
+
         break;
       default:
-        var filename = path.join(process.cwd() + '/docroot', uri);
-        drawd.serveFile(filename);
+        drawd.serveFile(path.join(process.cwd() + '/docroot', uri));
+
         break;
     }
   };
@@ -138,9 +103,11 @@ var drawd = function (req, res) {
   drawd.post = function () {
     switch (uri) {
       case '/canvas':
-        req.on('data', drawd.updateCanvas);
+        req.on('data', drawd.doCommand);
+
         break;
       default:
+
         break;
     }
   };
